8e08ae0842549c9a584dadc7e414b9801faff103
javiarmijo <100471908@alumnos.uc3m.es>
2025-06-27 16:51:58 +0200

    [WIP]

--- bindings/PySTS/solvers.cpp
+++ bindings/PySTS/solvers.cpp
@@ -11,4 +11,10 @@ using namespace roboticslab;
 void init_solvers(pybind11::module &m)
 {
     py::class_<MatrixExponential> exp(m, "MatrixExponential");
+    py::enum_<MatrixExponential::motion> motion_type(exp, "motion");
+    motion_type.value("ROTATION", MatrixExponential::motion::ROTATION);
+    motion_type.value("TRANSLATION", MatrixExponential::motion::TRANSLATION);
+
+    exp.def(py::init<MatrixExponential::motion, const KDL::Vector &, const KDL::Vector &>(),
+            py::arg("motionType"), py::arg("axis"), py::arg_v("origin", KDL::Vector::Zero(), "Vector.Zero"));
 }
